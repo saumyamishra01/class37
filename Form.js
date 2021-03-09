4f486554b7d6c6e1151fd096de3c7fe33e8ba9a7
saumyamishra01 <75878141+saumyamishra01@users.noreply.github.com>
2021-03-09 14:06:41 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,14 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+
+  
---
+++ Player.js
@@ -0,0 +1,23 @@
+class Player {
+  constructor(){
+    
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  
